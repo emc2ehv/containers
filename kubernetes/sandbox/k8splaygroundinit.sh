87209c25413ac07af40d744e379faf31454356b7
emc2ehv <mayank_sesame@yahoo.com>
2018-02-22 17:00:41 -0500

    Update k8splaygroundinit.sh

--- kubernetes/sandbox/k8splaygroundinit.sh
+++ kubernetes/sandbox/k8splaygroundinit.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 yum install -y wget
 kubeadm init --apiserver-advertise-address $(hostname -i) >> /tmp/kudeadminit.out
 chmod 777 /tmp/kudeadminit.out
@@ -14,21 +15,3 @@ awk '/kubeadm join/' /tmp/kudeadminit.out >> /tmp/kudeadminit.out
 clear
 cat /tmp/kudeadminit.out
 
-# Run on master, After workers are up
-# Crete nginx directory
-mkdir nginx
-cd nginx
-
-# Config Map
-wget https://raw.githubusercontent.com/emc2ehv/containers/master/kubernetes/sandbox/nginx/nginx.properties
-kubectl -n nginxspace create configmap nginx-config --from-file=./nginx.properties
-
-# nginx app
-wget https://raw.githubusercontent.com/emc2ehv/containers/master/kubernetes/sandbox/nginx/nginx-deployment.yaml
-wget https://raw.githubusercontent.com/emc2ehv/containers/master/kubernetes/sandbox/nginx/nginx-svc.yaml
-kubectl create namespace nginxspace
-kubectl -n nginxspace apply -f ./
-
-
-# Optional
-curl -L -s https://raw.githubusercontent.com/kubernetes/dashboard/master/src/deploy/recommended/kubernetes-dashboard.yaml  | sed 's/targetPort: 8443/targetPort: 8443\n  type: LoadBalancer/' | kubectl apply -f -
