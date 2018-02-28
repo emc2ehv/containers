571aa02c7a0b96c17aa974a0ac4d0e1e1155aa0e
emc2ehv <mayank_sesame@yahoo.com>
2018-02-28 16:04:45 -0500

    Update k8splaygroundinit.sh

--- kubernetes/sandbox/k8splaygroundinit.sh
+++ kubernetes/sandbox/k8splaygroundinit.sh
@@ -6,13 +6,9 @@ useradd kubeadmtest
 mkdir -p /home/kubeadmtest/.kube
 cp -i /etc/kubernetes/admin.conf /home/kubeadmtest/.kube/config
 chown -R kubeadmtest:kubeadmtest /home/kubeadmtest/.kube/
-sudo -u kubeadmtest bash << EOF
-kubectl apply -n kube-system -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
-EOF
+sudo -u kubeadmtest kubectl apply -n kube-system -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
 echo "--------------------------------------------------------------------------------------------------" >> /tmp/kudeadminit.out
 echo "Your Kubernetes master is ready. Please allow up to a minute for the Cluster and WebUI to come up" >> /tmp/kudeadminit.out
 echo "Your worker nodes can join this master by running the following command as root" >> /tmp/kudeadminit.out
 awk '/kubeadm join/' /tmp/kudeadminit.out >> /tmp/kudeadminit.out
-su - kubeadmtest
-cat /tmp/kudeadminit.out
 
