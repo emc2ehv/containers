d5f4f817e2f99cc749a2eb03237f283e800bf6dd
Mayank Patel <mayank_sesame@yahoo.com>
2018-03-21 12:14:20 -0400

    Update asciinemacentos7.sh

--- misc/scripts/asciinemacentos7.sh
+++ misc/scripts/asciinemacentos7.sh
@@ -4,11 +4,5 @@
 yum install -y https://centos7.iuscommunity.org/ius-release.rpm
 yum install -y git python36u
 
-# Make sure locale is UTF-8
-export LANG=en_US.utf-8
-export LC_ALL=en_US.utf-8
-
 # Get asciinema
 git clone https://github.com/asciinema/asciinema.git
-cd asciinema
-python3.6 -m asciinema --version
