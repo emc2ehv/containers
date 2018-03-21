c6548931b457183d438aefd373c3d8f2f019a41d
Mayank Patel <mayank_sesame@yahoo.com>
2018-03-21 17:38:32 -0400

    add git

--- kubernetes/sandbox/k8splaygroundinit.sh
+++ kubernetes/sandbox/k8splaygroundinit.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-yum install -y wget sudo
+yum install -y wget sudo git
 kubeadm init --apiserver-advertise-address $(hostname -i) >> /tmp/kudeadminit.out
 chmod 777 /tmp/kudeadminit.out
 useradd kubeadmtest
@@ -8,7 +8,7 @@ cp -i /etc/kubernetes/admin.conf /home/kubeadmtest/.kube/config
 chown -R kubeadmtest:kubeadmtest /home/kubeadmtest/.kube/
 sudo -u kubeadmtest kubectl apply -n kube-system -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
 echo "--------------------------------------------------------------------------------------------------" >> /tmp/kudeadminit.out
-echo "Your Kubernetes master is ready. Please allow up to a minute for the Cluster and WebUI to come up" >> /tmp/kudeadminit.out
+echo "Your Kubernetes master is ready. Please allow up to a minute for the Cluster to come up" >> /tmp/kudeadminit.out
 echo "Your worker nodes can join this master by running the following command as root" >> /tmp/kudeadminit.out
 awk '/kubeadm join/' /tmp/kudeadminit.out >> /tmp/kudeadminit.out
 
